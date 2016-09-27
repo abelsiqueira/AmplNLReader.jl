756c4b2879a9857e1f72b534b4a10ee94cda0675
Dominique Orban <dominique.orban@gmail.com>
2016-09-27 17:20:44 -0400

    attempt msbuild on Windows

--- deps/build.jl
+++ deps/build.jl
@@ -4,27 +4,22 @@ using Compat
 @BinDeps.setup
 
 libasl = library_dependency("libasl", aliases=["libasl.3", "libasl.3.1.0"])
-libmp = library_dependency("libmp", aliases=["libmp.3", "libmp.3.1.0"])
 
-# Hopeless.
-# @osx_only begin
-#   using Homebrew
-#   provides(Homebrew.HB, "homebrew/science/asl", [libasl, libmp], os = :Darwin)
-#   push!(Libdl.DL_LOAD_PATH, joinpath(Homebrew.prefix("asl"), "lib"))
-# end
+@static if is_apple()
+  using Homebrew
+  provides(Homebrew.HB, "homebrew/science/asl", libasl, os = :Darwin)
+end
 
 # Uncomment when there is a deb for the ASL.
-# provides(AptGet, "libasl-dev", [libasl, libmp], os = :Linux)
+# provides(AptGet, "libasl-dev", libasl, os = :Linux)
 
-# Outdated!
-@windows_only begin
-  using WinRPM
-  provides(WinRPM.RPM, "ampl-mp", [libasl, libmp], os = :Windows)
+@static if is_windows()
+  push!(BinDeps.defaults, BinDeps.BuildProcess)
 end
 
 provides(Sources,
          URI("https://github.com/ampl/mp/archive/3.1.0.tar.gz"),
-         [libasl, libmp],
+         libasl,
          SHA="587c1a88f4c8f57bef95b58a8586956145417c8039f59b1758365ccc5a309ae9",
          unpacked_dir="mp-3.1.0")
 
@@ -44,6 +39,18 @@ provides(SimpleBuild,
                   `make install`
                 end)
              end)
-          end), [libasl, libmp], os = :Unix)
+          end), libasl, os = :Unix)
+
+provides(SimpleBuild,
+         (@build_steps begin
+            GetSources(libasl)
+            (@build_steps begin
+               ChangeDirectory(srcdir)
+               (@build_steps begin
+                  `cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_INSTALL_RPATH=$prefix/lib -DBUILD_SHARED_LIBS=True`
+                  `msbuild MP.sln`
+                end)
+             end)
+          end), libasl, os = :Windows)
 
 @BinDeps.install @compat Dict(:libasl => :libasl)
